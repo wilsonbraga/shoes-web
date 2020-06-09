9fb6ee37f36b328e7376a08d31c2db3bde25fcf7
wilson <wilsonrodriguesbraga@gmail.com>
2020-06-09 11:38:18 -0300

    estilizando home

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -22,7 +22,5 @@ export default function Header() {
 
       </Cart>
     </Container>
-
-
   );
 }
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,88 @@
 import React from 'react';
+import { MdAddShoppingCart } from 'react-icons/md'
 
-// import { Container } from './styles';
+ import { ProductList } from './styles';
 
 export default function Home() {
-  return <h1>Home</h1>;
+  return (
+  <ProductList>
+    <li>
+      <img src="https://images.lojanike.com.br/320x320/produto/217943_1996808_20200303171118.jpg" alt=""/>
+      <strong>Tênis Nike Revolution 5 Masculino</strong>
+      <span>R$180,00</span>
+
+      <button type="button">
+        <div>
+          <MdAddShoppingCart size={16} color="#fff" /> 3
+        </div>
+        <span>ADICIONAR AO CARRINHO</span>
+      </button>
+    </li>
+
+    <li>
+      <img src="https://images.lojanike.com.br/320x320/produto/217943_1996808_20200303171118.jpg" alt=""/>
+      <strong>Tênis Nike Revolution 5 Masculino</strong>
+      <span>R$180,00</span>
+
+      <button type="button">
+        <div>
+          <MdAddShoppingCart size={16} color="#fff" /> 3
+        </div>
+        <span>ADICIONAR AO CARRINHO</span>
+      </button>
+    </li>
+
+    <li>
+      <img src="https://images.lojanike.com.br/320x320/produto/217943_1996808_20200303171118.jpg" alt=""/>
+      <strong>Tênis Nike Revolution 5 Masculino</strong>
+      <span>R$180,00</span>
+
+      <button type="button">
+        <div>
+          <MdAddShoppingCart size={16} color="#fff" /> 3
+        </div>
+        <span>ADICIONAR AO CARRINHO</span>
+      </button>
+    </li>
+
+    <li>
+      <img src="https://images.lojanike.com.br/320x320/produto/217943_1996808_20200303171118.jpg" alt=""/>
+      <strong>Tênis Nike Revolution 5 Masculino</strong>
+      <span>R$180,00</span>
+
+      <button type="button">
+        <div>
+          <MdAddShoppingCart size={16} color="#fff" /> 3
+        </div>
+        <span>ADICIONAR AO CARRINHO</span>
+      </button>
+    </li>
+
+    <li>
+      <img src="https://images.lojanike.com.br/320x320/produto/217943_1996808_20200303171118.jpg" alt=""/>
+      <strong>Tênis Nike Revolution 5 Masculino</strong>
+      <span>R$180,00</span>
+
+      <button type="button">
+        <div>
+          <MdAddShoppingCart size={16} color="#fff" /> 3
+        </div>
+        <span>ADICIONAR AO CARRINHO</span>
+      </button>
+    </li>
+
+    <li>
+      <img src="https://images.lojanike.com.br/320x320/produto/217943_1996808_20200303171118.jpg" alt=""/>
+      <strong>Tênis Nike Revolution 5 Masculino</strong>
+      <span>R$180,00</span>
+
+      <button type="button">
+        <div>
+          <MdAddShoppingCart size={16} color="#fff" /> 3
+        </div>
+        <span>ADICIONAR AO CARRINHO</span>
+      </button>
+    </li>
+  </ProductList>
+  );
 }
