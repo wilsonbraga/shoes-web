4df2fae9d0d35e7406f697635aacb4f90cbb0048
wilson <wilsonrodriguesbraga@gmail.com>
2020-06-10 00:12:04 -0300

    remover produto

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -3,7 +3,7 @@ import { MdRemoveCircleOutline, MdAddCircleOutline, MdDelete } from 'react-icons
 import { connect } from 'react-redux';
 import { Container, ProductTable, Total } from './estyles';
 
-function Cart({ cart }) {
+function Cart({ cart, dispatch }) {
   return (
     <Container>
       <ProductTable>
@@ -41,7 +41,8 @@ function Cart({ cart }) {
                 <strong>R$360,00</strong>
               </td>
               <td>
-                <button type="button">
+                <button type="button" 
+                  onClick={() => dispatch({ type: 'REMOVE_FROM_CART', id: product.id })}>
                   <MdDelete size={20} color="#1919FF" />
                 </button>
               </td>
